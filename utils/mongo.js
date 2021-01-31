ca6496e6c0e7a93b9d165487b81ab638e5252627
Lokesh Patel <32188450+lokesh58@users.noreply.github.com>
2021-01-31 19:39:00 +0530

    utility to connected to mongoDB

---
+++ utils/mongo.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+
+const mongoPath = `mongodb+srv://fuhua:${process.env.DB_PASS}@faq.nhcks.mongodb.net/faqs?retryWrites=true&w=majority`;
+
+module.exports = async() => {
+    await mongoose.connect(mongoPath, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    });
+    return mongoose;
+}
