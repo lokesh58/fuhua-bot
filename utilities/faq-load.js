e1d6d8bce1672d2e7b54d32a8a9248af51f6f9c3
Lokesh Patel <32188450+lokesh58@users.noreply.github.com>
2021-02-03 09:07:23 +0530

    new utility loads all faqs in memory after startup

---
+++ utilities/faq-load.js
@@ -0,0 +1,17 @@
+const cache = require('@cache/faq-cache.js');
+const mongo = require('@utils/mongo.js');
+const faqSchema = require('@schemas/faq-schema.js');
+
+module.exports = async () => {
+    await mongo().then(async (mongoose) => {
+        try{
+            const faqs = await faqSchema.find({});
+            for(const faq of faqs){
+                cache.push({name: faq.name,
+                            description: faq.description});
+            }
+        } finally {
+            mongoose.connection.close();
+        }
+    }).catch(console.error);
+}
