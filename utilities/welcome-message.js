0489bda7e0a5155d485cf4892e23ad7f2215221b
Lokesh Patel <32188450+lokesh58@users.noreply.github.com>
2021-02-03 09:06:01 +0530

    adds valkyrija role to new joining members

--- utilities/welcome-message.js
+++ utilities/welcome-message.js
@@ -6,6 +6,10 @@ module.exports = client => {
         const rulesChannelID = '801755778338783242';
         const rulesChannel = member.guild.channels.cache.get(rulesChannelID);
 
+        const {guild} = member;
+        const valkRole = guild.roles.cache.find(role => role.name === 'Valkyrija');
+        member.roles.add(valkRole);
+
         const message = `Welcome <@${member.id}> to St. Freya High. Please check out ${rulesChannel.toString()}`;
         welcomeChannel.send(message);
     })
