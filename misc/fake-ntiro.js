ca95bef0504acbcb6e3c5d13eddda35371654d5d
Lokesh Patel <32188450+lokesh58@users.noreply.github.com>
2021-02-08 17:48:30 +0530

    added nqn like nitro effect

---
+++ misc/fake-ntiro.js
@@ -0,0 +1,26 @@
+module.exports = async (message) => {
+    let resend = false;
+    const args = message.content.split(' ');
+    const newMsg = [];
+    for(const word of args){
+        if(word.startsWith(':') && word.endsWith(':')){
+            const name = word.slice(1,-1).toLowerCase();
+            const emote = message.client.emojis.cache.find(emoji => emoji.name.toLowerCase() === name);
+            if(emote){
+                newMsg.push(`${emote}`);
+                resend = true;
+            }else{
+                newMsg.push(word);
+            }
+        }else{
+            newMsg.push(word);
+        }
+    }
+    if(resend){
+        message.delete();
+        const fakeUser = await message.channel.createWebhook(message.member.displayName, {
+            avatar: message.author.displayAvatarURL()
+        })
+        fakeUser.send(newMsg.join(' '));
+    }
+}
