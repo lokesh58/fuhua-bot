a81fa8cb7110eabc1e041f518bf180e7cdb89298
Lokesh Patel <32188450+lokesh58@users.noreply.github.com>
2021-02-09 19:08:33 +0530

    removed creator command

--- commands/creator.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    name: 'creator',
-    description: 'this is command to praise my creator!',
-    format:'!creator',
-    execute(message, args){
-        message.channel.send(`lokesh58 makes the best bots <:fuhuaSmile:800929579199496233>`);
-    }
-}
