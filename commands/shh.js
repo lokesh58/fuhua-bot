94e78cc8a723512fb686fb4aedcab5c751ecc2fd
Lokesh Patel <32188450+lokesh58@users.noreply.github.com>
2021-02-09 19:31:46 +0530

    no message sent after using the command

--- commands/shh.js
+++ commands/shh.js
@@ -6,7 +6,6 @@ module.exports = {
         if(!message.member.hasPermission('MANAGE_CHANNELS'))
             return message.reply('you don\'t have the permission to use this command');
         message.react('🔇');
-        message.channel.send('Please stop talking and maintain discipline!');
         message.channel.updateOverwrite(message.channel.guild.roles.everyone, { SEND_MESSAGES: false });
     }
 }
--- commands/thanks.js
+++ commands/thanks.js
@@ -6,7 +6,6 @@ module.exports = {
         if(!message.member.hasPermission('MANAGE_CHANNELS'))
             return message.reply('you don\'t have the permission to use this command');
         message.react('🔈');
-        message.channel.send('Maintain Discipline from now on <:fuhuaSmile:800929579199496233>');
         message.channel.updateOverwrite(message.channel.guild.roles.everyone, { SEND_MESSAGES: null });
     }
 }
