e40c68c6b925154680fbba6a692283c7790a4f80
Lokesh Patel <32188450+lokesh58@users.noreply.github.com>
2021-02-09 19:08:49 +0530

    command load is now a utility

--- index.js
+++ index.js
@@ -1,49 +1,18 @@
 require('dotenv').config();
 require('module-alias/register');
-const Discord = require('discord.js');
+const {Client} = require('discord.js');
 
-const client = new Discord.Client({partials: ["MESSAGE", "CHANNEL", "REACTION"]});
+const client = new Client({partials: ["MESSAGE", "CHANNEL", "REACTION"]});
 
 const prefix = '!';
 
-const fs = require('fs');
-
-client.commands = new Discord.Collection();
-
-const commandFiles = fs.readdirSync('./commands/').filter(file=>file.endsWith('.js'));
-for(const file of commandFiles){
-    const command = require(`./commands/${file}`);
-
-    client.commands.set(command.name, command);
-}
-
-const utilityFiles = fs.readdirSync('./utilities/').filter(file=>file.endsWith('.js'));
-
 client.once('ready', ()=>{
+    const utilityFiles = fs.readdirSync('./utilities/').filter(file=>file.endsWith('.js'));
     for(const file of utilityFiles){
         const utility = require(`./utilities/${file}`);
         utility(client);
     }
     console.log('Fu Hua is online');
-});
-
-const fakeNitro = require('./misc/fake-ntiro')
-
-client.on('message', message=>{
-    if(message.author.bot) return;
-    if(message.content.startsWith(prefix)){
-        const args = message.content.slice(prefix.length).split(/ +/);
-        const command = args.shift().toLowerCase();
-    
-        const cmd = client.commands.get(command);
-        if(cmd){
-            cmd.execute(message, args);
-        }else{
-            message.reply('This is not a valid command!\nHint: To get list of all valid commands, use !list');
-        }
-    }else{
-        //fakeNitro(message);
-    }
 });
 
 client.login(process.env.BOT_TOKEN);
---
+++ utilities/load-commands.js
@@ -0,0 +1,25 @@
+const fs = require('fs')
+const {Collection} = require('discord.js')
+
+module.exports = (client) => {
+    client.commands = new Collection()
+    const commandFiles = fs.readdirSync('../commands/').filter(file=>file.endsWith('.js'))
+    for(const file of commandFiles){
+        const command = require(`../commands/${file}`)
+        client.commands.set(command.name, command)
+    }
+    client.on('message', message=>{
+        if(message.author.bot) return;
+        if(message.content.startsWith(prefix)){
+            const args = message.content.slice(prefix.length).split(/ +/)
+            const command = args.shift().toLowerCase()
+        
+            const cmd = client.commands.get(command)
+            if(cmd){
+                cmd.execute(message, args)
+            }else{
+                message.reply('This is not a valid command!\nHint: To get list of all valid commands, use !list')
+            }
+        }
+    });
+}
