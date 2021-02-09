2cce26783f845c464532f1613398a4a47378b247
Lokesh Patel <32188450+lokesh58@users.noreply.github.com>
2021-02-09 18:57:34 +0530

    bot joining is annouced

--- utilities/welcome-message.js
+++ utilities/welcome-message.js
@@ -1,18 +1,19 @@
 module.exports = client => {
     client.on('guildMemberAdd', member => {
+        const {guild} = member
+
         const welcomeChannelID = '790503598928560128';
-        const welcomeChannel = member.guild.channels.cache.get(welcomeChannelID);
+        const welcomeChannel = guild.channels.cache.get(welcomeChannelID);
+
+        const annoucementChannelID = '802927957160230942'
+        const annoucementChannel = guild.channels.cache.get(annoucementChannelID);
 
         const rulesChannelID = '801755778338783242';
-        const rulesChannel = member.guild.channels.cache.get(rulesChannelID);
+        const rulesChannel = guild.channels.cache.get(rulesChannelID);
 
-        const {guild} = member;
-        const valkRole = guild.roles.cache.find(role => role.name === 'Valkyrija');
-        const botRole = guild.roles.cache.find(role => role.name === 'Bot');
         if(member.user.bot){
-            member.roles.add(botRole);
+            annoucementChannel.send(`${member.displayName} has been added to the server!`)
         }else{
-            member.roles.add(valkRole);
             const message = `Welcome <@${member.id}> to St. Freya High. Please check out ${rulesChannel.toString()}`;
             welcomeChannel.send(message);
         }
