74deb74869615993a71abc681e40c61b5d11c9fa
Lokesh Patel <32188450+lokesh58@users.noreply.github.com>
2021-06-06 14:17:43 +0530

    Fixed bug due to which bot crashed

--- utilities/editsnipe.js
+++ utilities/editsnipe.js
@@ -3,7 +3,7 @@ const cache = require('@cache/editsnipe-cache')
 module.exports = (client) => {
     client.on('messageUpdate', (oldMessage, newMessage) => {
         const {channel, author, content} = oldMessage
-        if (author.bot) return
+        if (author.bot || channel.type === 'dm') return
         cache[channel.id] = {
             user: author.tag,
             content: content
--- utilities/snipe.js
+++ utilities/snipe.js
@@ -3,7 +3,7 @@ const cache = require('@cache/snipe-cache')
 module.exports = (client) => {
     client.on('messageDelete', message => {
         const {channel, author, content} = message
-        if (author.bot) return
+        if (author.bot || channel.type === 'dm') return
         cache[channel.id] = {
             user: author.tag,
             content: content
